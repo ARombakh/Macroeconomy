388526e00a40a267669703c0128acddef8d59305
Artyom Rombakh <artromb@mail.ru>
2025-12-23 12:15:44 +0300

    Getters and setters in Worker added

--- src/macroeconomy/Worker.java
+++ src/macroeconomy/Worker.java
@@ -15,4 +15,52 @@ public class Worker {
     private int foodCapacity;
     private int foodConsumptMnth;
     private int labor;
+
+    public double getProductivity() {
+        return productivity;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getDeposit() {
+        return deposit;
+    }
+
+    public int getFoodCapacity() {
+        return foodCapacity;
+    }
+
+    public int getFoodConsumptMnth() {
+        return foodConsumptMnth;
+    }
+
+    public int getLabor() {
+        return labor;
+    }
+
+    public void setProductivity(double productivity) {
+        this.productivity = productivity;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void setDeposit(int deposit) {
+        this.deposit = deposit;
+    }
+
+    public void setFoodCapacity(int foodCapacity) {
+        this.foodCapacity = foodCapacity;
+    }
+
+    public void setFoodConsumptMnth(int foodConsumptMnth) {
+        this.foodConsumptMnth = foodConsumptMnth;
+    }
+
+    public void setLabor(int labor) {
+        this.labor = labor;
+    }
 }
